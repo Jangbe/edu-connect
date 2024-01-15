533adc5b137fccd4f5c8da1a912da56c741b87c4
estradax <azharumuhammad@gmail.com>
2024-01-16 00:37:15 +0700

    feat: add location information

--- lib/bloc/school_marker_bloc.dart
+++ lib/bloc/school_marker_bloc.dart
@@ -1,5 +1,6 @@
 import 'package:basreng/model/location_marker.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:geocoding/geocoding.dart';
 
 class SchoolMarkerBloc extends Bloc<SchoolMarkerEvent, SchoolMarkerState> {
   SchoolMarkerBloc() : super(SchoolMarkerState(markers: [])) {
@@ -8,14 +9,36 @@ class SchoolMarkerBloc extends Bloc<SchoolMarkerEvent, SchoolMarkerState> {
 
       emit(SchoolMarkerState(markers: results));
     });
+
+    on<GetPlacemarkSchoolEvent>((event, emit) async {
+      final placemarks = await placemarkFromCoordinates(event.lat, event.long);
+      final state = SchoolMarkerState(markers: []);
+      if (placemarks.isNotEmpty) {
+        state.placemark = placemarks[0];
+      }
+
+      emit(state);
+    });
   }
 }
 
 class SchoolMarkerEvent {}
+
 class FetchAllSchoolMarkerEvent extends SchoolMarkerEvent {}
+
+class GetPlacemarkSchoolEvent extends SchoolMarkerEvent {
+  final double lat;
+  final double long;
+
+  GetPlacemarkSchoolEvent({
+    required this.lat,
+    required this.long,
+  });
+}
 
 class SchoolMarkerState {
   final List<LocationMarker> markers;
+  Placemark? placemark;
 
   SchoolMarkerState({required this.markers});
 }
--- lib/ui/school_information_screen.dart
+++ lib/ui/school_information_screen.dart
@@ -1,8 +1,13 @@
+import 'package:basreng/bloc/school_marker_bloc.dart';
 import 'package:basreng/model/location_marker.dart';
+import 'package:basreng/ui/apply_for_job_screen.dart';
+import 'package:basreng/widget/my_back_appbar.dart';
 import 'package:basreng/widget/my_card.dart';
 import 'package:basreng/widget/my_headbar.dart';
+import 'package:basreng/widget/my_rounded_button.dart';
 import 'package:basreng/widget/my_row_card.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SchoolInformationScreen extends StatelessWidget {
   final LocationMarker locationMarker;
@@ -12,25 +17,9 @@ class SchoolInformationScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: GestureDetector(
-          onTap: () => Navigator.of(context).pop(),
-          child: Image(
-            image: AssetImage('images/chv-left.png'),
-          ),
-        ),
-        leadingWidth: 24,
-        titleSpacing: 4,
-        title: Text(
-          'School Information',
-          style: TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        shadowColor: Colors.black,
-        elevation: 1,
-        backgroundColor: Colors.white,
+      appBar: myBackAppBar(
+        context,
+        title: 'School Information',
       ),
       body: SingleChildScrollView(
         child: Column(
@@ -40,16 +29,30 @@ class SchoolInformationScreen extends StatelessWidget {
             ),
             Padding(
               padding: EdgeInsets.symmetric(horizontal: 12),
-              child: MyRowCard(
-                title: locationMarker.name,
-                description: 'Location: City, Country',
-                image: Image(
-                  image: AssetImage('images/school-avatar.png'),
-                  width: 40,
-                ),
-                action: SizedBox(
-                  width: 0,
-                  height: 0,
+              child: BlocProvider(
+                create: (context) => SchoolMarkerBloc()
+                  ..add(
+                    GetPlacemarkSchoolEvent(
+                      lat: double.parse(locationMarker.lat),
+                      long: double.parse(
+                        locationMarker.long,
+                      ),
+                    ),
+                  ),
+                child: BlocBuilder<SchoolMarkerBloc, SchoolMarkerState>(
+                  builder: (context, state) => MyRowCard(
+                    title: locationMarker.name,
+                    description:
+                        'Location: ${state.placemark?.locality}, ${state.placemark?.country}',
+                    image: Image(
+                      image: AssetImage('images/school-avatar.png'),
+                      width: 40,
+                    ),
+                    action: SizedBox(
+                      width: 0,
+                      height: 0,
+                    ),
+                  ),
                 ),
               ),
             ),
@@ -84,33 +87,15 @@ class SchoolInformationScreen extends StatelessWidget {
               children: [
                 Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 12),
-                  child: Container(
-                    height: 42,
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 12, vertical: 10),
-                    decoration: ShapeDecoration(
-                      color: Color(0xFFABE2F9),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(22),
-                      ),
-                    ),
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: const [
-                        Text(
-                          'Apply Now',
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 16,
-                            fontFamily: 'Roboto',
-                            fontWeight: FontWeight.w500,
-                            height: 0.09,
-                          ),
+                  child: MyRoundedButton(
+                    title: 'Apply Now',
+                    onTap: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) => ApplyForJobScreen(),
                         ),
-                      ],
-                    ),
+                      );
+                    },
                   ),
                 ),
               ],
